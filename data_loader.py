bdf7c0c576818d8525bfa2746ea55ab127388973
calvincoronado <104296872+calvincoronado@users.noreply.github.com>
2023-11-29 22:37:24 +0800

    2nd Model added
    need pa ng isa, plus visualization pa

--- data_loader.py
+++ data_loader.py
@@ -27,7 +27,7 @@ class DataLoader(object):
 
         # TODO: Use np.random.shuffle() to shuffles the indices in self.indices
         np.random.shuffle(self.indices)
-
+        
     def get_batch(self, mode='train'):
         """Returns self.X and self.y divided into different batches of size
         self.batch_size according to the shuffled self.indices.
@@ -60,14 +60,14 @@ class DataLoader(object):
             # TODO: Check if we can still get self.batch_size from the
             # remaining indices starting from index i. Edit the condition
             # below.
-            if (i + self.batch_size) < len(self.indices) :
+            if i + self.batch_size <= len(self.indices):
                 indices = self.indices[i:i + self.batch_size]
 
             # TODO: Else, just get the remaining indices from index i until the
             # last element in the list. Edit the statement inside the else
             # block.
             else:
-                indices = self.indices[i:len(self.indices)]
+                indices = self.indices[i:]
 
             X_batch.append(self.X[indices])
             y_batch.append(self.y[indices])
